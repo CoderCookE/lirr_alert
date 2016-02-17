a5f90477468d93b3d7433ad237b2b3ae109d5b32
Eric Cook <ejcook111@gmail.com>
2016-02-17 12:15:57 -0500

    initial commit, /lirr endpoint, poll lirr page every 15 seconds to updates

---
+++ Makefile
@@ -0,0 +1,20 @@
+APP_NAME=lirr_alert
+
+default: bin
+
+bin:
+	mkdir -p bin
+	godep go build -i -o ./bin/$(APP_NAME)
+
+test:
+	godep go test -timeout=10s -v
+
+format:
+	git ls-files | grep '.go$$' | xargs gofmt -w
+
+deps:
+	go get github.com/tools/godep
+	GOPATH=`pwd`/Godeps/_workspace godep restore
+
+.PHONY: default bin test format deps
+
---
+++ main.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/mux"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"regexp"
+	"strings"
+	"time"
+)
+
+func main() {
+	go startPolling()
+	router := mux.NewRouter().StrictSlash(true)
+	router.HandleFunc("/lirr", Lirr)
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func Lirr(w http.ResponseWriter, r *http.Request) {
+	fmt.Sprintf("http://www.movable-ink-7158.com/p/rp/60e6a4c03b713777.png?cache_buster=%s", time.Now().String())
+}
+
+type AlertChecker struct {
+	Raw   string
+	lines map[string]*Line
+}
+
+type Line struct {
+	name   string
+	status string
+	text   string
+}
+
+func startPolling() {
+	a := AlertChecker{}
+	a.lines = make(map[string]*Line)
+
+	for {
+		<-time.After(15 * time.Second)
+		go a.CheckAlert()
+	}
+}
+
+func (a *AlertChecker) CheckAlert() {
+	resp, _ := http.Get("http://web.mta.info/status/serviceStatus.txt")
+	defer resp.Body.Close()
+	rawData, _ := ioutil.ReadAll(resp.Body)
+	htmlData := string(rawData)
+	nameRegExp, _ := regexp.Compile("<name>(.*)</name>")
+	statusRegExp, _ := regexp.Compile("<status>(.*)</status>")
+	textRegExp, _ := regexp.Compile("<text>(.*)</text>")
+	if htmlData != a.Raw {
+		a.Raw = htmlData
+		rawTrains := fmt.Sprintf("%q", strings.SplitAfter(htmlData, "LIRR>")[1])
+		s := strings.SplitAfter(rawTrains, "</line>")
+
+		for _, v := range s {
+			var currentText string
+			var currentStatus string
+
+			if a.lines[v] != nil {
+				currentText = a.lines[v].text
+				currentStatus = a.lines[v].status
+			}
+
+			status := string(statusRegExp.Find([]byte(v)))
+			text := string(textRegExp.Find([]byte(v)))
+			name := string(nameRegExp.Find([]byte(v)))
+
+			if currentStatus != status || currentText != text {
+
+				log.Printf("%s", a.lines[v])
+
+				a.lines[v] = &Line{name, status, text}
+
+				log.Printf(a.lines[v].text, a.lines[v].status)
+			}
+		}
+	}
+}
